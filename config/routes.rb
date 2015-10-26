464b5f1c2b9e7491d39359eaeb570f78a8e80623
Sam Uherek <samuherekbiz@gmail.com>
2015-10-26 21:17:35 +0800

    Added a featured product

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
