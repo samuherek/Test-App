a8dbe4f2581d762d5627aec9fb18aa5be1e4ad61
Sam Uherek <samuherekbiz@gmail.com>
2016-01-01 19:10:09 +0800

    added 6.3 functional tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Product do
+
+  before do
+    @product = Product.create!(name: "race bike")
+    @user = User.create!(email: "test@email.com", password: "12345678")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful Bike")
+    @product.comments.create!(rating: 3, user: @user, body: "OK Bike")
+    @product.comments.create!(rating: 5, user: @user, body: "Best Bike")
+  end
+
+  it "returns the average rating of all comments" do
+    expect(@product.average_rating).to eq "3.0"
+  end
+
+  it "validates the prducts" do
+    expect(@product).not_to be_valid
+  end
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+  # Devise helperes to use in the rspec
+  require 'spec_helper'
+  require 'rspec/rails'
+  # note: require 'devise' after require 'rspec/rails'
+  require 'devise'
+
+  RSpec.configure do |config|
+    config.include Devise::TestHelpers, :type => :controller
+  end
+
+
 end
